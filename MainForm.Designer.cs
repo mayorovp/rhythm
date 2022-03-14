a371072dfc7a27cd1e2c2925f3147f99c435cfae
Майоров Павел <mayorovp@gmail.com>
2022-03-14 14:06:21 +0500

    Добавил кнопки паузы и перемотки

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Rhythm.ViewModel viewModel2 = new Rhythm.ViewModel();
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.playback = new Rhythm.PlaybackControl();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPast1 = new System.Windows.Forms.Button();
+            this.btnPast10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // propertyGrid
@@ -72,6 +76,25 @@
             // playback
             // 
             this.playback.BackColor = System.Drawing.Color.White;
+            viewModel2.BackgroundColor = System.Drawing.Color.White;
+            viewModel2.BorderWidth = 3;
+            viewModel2.ComplementaryColor = System.Drawing.Color.Yellow;
+            viewModel2.ExerciseCount = 4;
+            viewModel2.ExerciseFirst = 1;
+            viewModel2.ExerciseInterval = 6;
+            viewModel2.ForegroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            viewModel2.Frequency = 76;
+            viewModel2.Length = 8;
+            viewModel2.PointerColor = System.Drawing.Color.Red;
+            viewModel2.PointerSize = 3;
+            viewModel2.SeriesCount = 12;
+            viewModel2.SeriesInterval = 4;
+            viewModel2.StartDelay = 7;
+            viewModel2.TextColor = System.Drawing.Color.Black;
+            viewModel2.TextFont = new System.Drawing.Font("Arial", 14.25F);
+            viewModel2.VisibleRangeAfter = System.TimeSpan.Parse("00:00:15");
+            viewModel2.VisibleRangeBefore = System.TimeSpan.Parse("00:00:02");
+            this.playback.Config = viewModel2;
             this.playback.Dock = System.Windows.Forms.DockStyle.Fill;
             this.playback.ElapsedTime = 0D;
             this.playback.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
@@ -85,11 +108,47 @@
             // 
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPause.Location = new System.Drawing.Point(632, 415);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Пауза";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPast1
+            // 
+            this.btnPast1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPast1.Location = new System.Drawing.Point(551, 415);
+            this.btnPast1.Name = "btnPast1";
+            this.btnPast1.Size = new System.Drawing.Size(75, 23);
+            this.btnPast1.TabIndex = 5;
+            this.btnPast1.Text = "-1 сек";
+            this.btnPast1.UseVisualStyleBackColor = true;
+            this.btnPast1.Click += new System.EventHandler(this.btnPast1_Click);
+            // 
+            // btnPast10
+            // 
+            this.btnPast10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPast10.Location = new System.Drawing.Point(470, 415);
+            this.btnPast10.Name = "btnPast10";
+            this.btnPast10.Size = new System.Drawing.Size(75, 23);
+            this.btnPast10.TabIndex = 6;
+            this.btnPast10.Text = "-10 сек";
+            this.btnPast10.UseVisualStyleBackColor = true;
+            this.btnPast10.Click += new System.EventHandler(this.btnPast10_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPast10);
+            this.Controls.Add(this.btnPast1);
+            this.Controls.Add(this.btnPause);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.playback);
@@ -107,6 +166,9 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPast1;
+        private System.Windows.Forms.Button btnPast10;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,12 +7,14 @@ namespace Rhythm
     public partial class MainForm : Form
     {
         private readonly Stopwatch stopwatch = new Stopwatch();
+        private TimeSpan rewind;
 
         public MainForm()
         {
             InitializeComponent();
 
             propertyGrid.SelectedObject = playback.Config;
+            State = PlaybackState.Stopped;
         }
 
         private void playback_TimePerPixelChanged(object sender, System.EventArgs e)
@@ -22,33 +24,51 @@ namespace Rhythm
 
         private void btnStart_Click(object sender, System.EventArgs e)
         {
-            stopwatch.Restart();
-            timer.Enabled = true;
-            playback.ElapsedTime = 0;
-
-            propertyGrid.Visible = false;
-            btnStart.Enabled = false;
-            btnStop.Enabled = true;
+            if (State == PlaybackState.Stopped)
+            {
+                stopwatch.Restart();
+                rewind = TimeSpan.Zero;
+                playback.ElapsedTime = 0;
+            }
+            else
+            {
+                stopwatch.Start();
+            }
+            State = PlaybackState.Started;
         }
 
         private void btnStop_Click(object sender, System.EventArgs e)
         {
             stopwatch.Stop();
-            timer.Enabled = false;
+            State = PlaybackState.Stopped;
+        }
 
-            propertyGrid.Visible = true;
-            btnStart.Enabled = true;
-            btnStop.Enabled = false;
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+            State = PlaybackState.Paused;
+        }
+
+        private void btnPast1_Click(object sender, EventArgs e)
+        {
+            rewind += TimeSpan.FromSeconds(1);
+            playback.ElapsedTime = stopwatch.ElapsedMilliseconds - rewind.TotalMilliseconds;
+        }
+
+        private void btnPast10_Click(object sender, EventArgs e)
+        {
+            rewind += TimeSpan.FromSeconds(10);
+            playback.ElapsedTime = stopwatch.ElapsedMilliseconds - rewind.TotalMilliseconds;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            playback.ElapsedTime = stopwatch.ElapsedMilliseconds;
+            playback.ElapsedTime = stopwatch.ElapsedMilliseconds - rewind.TotalMilliseconds;
 
             if (playback.IsEnded)
             {
                 stopwatch.Stop();
-                timer.Enabled = false;
+                State = PlaybackState.Stopped;
             }
         }
 
@@ -57,6 +77,36 @@ namespace Rhythm
             playback.Config.Save();
 
             base.OnClosed(e);
+        }
+
+        enum PlaybackState
+        {
+            Stopped, Started, Paused
+        }
+        private PlaybackState _state;
+        private PlaybackState State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+
+                timer.Enabled = value == PlaybackState.Started;
+
+                btnStart.Visible = value != PlaybackState.Started;
+                btnPause.Visible = value == PlaybackState.Started;
+                btnStart.Visible = true;
+                btnPast1.Visible = value != PlaybackState.Stopped;
+                btnPast10.Visible = value != PlaybackState.Stopped;
+
+                btnStart.Enabled = true;
+                btnPause.Enabled = true;
+                btnPast1.Enabled = true;
+                btnPast10.Enabled = true;
+                btnStop.Enabled = value != PlaybackState.Stopped;
+
+                propertyGrid.Visible = value == PlaybackState.Stopped;
+            }
         }
     }
 }
